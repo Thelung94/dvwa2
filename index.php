097a26bb02d0bc34f5824129b3b8dfdce0b7e5f8
Thelung94 <dahlan.tahir.cs110@gmail.com>
2023-02-22 15:50:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,20 @@
 <?php
-$mysqli = new mysqli("localhost","root","","esmash");
+// $mysqli = new mysqli("localhost","root","","esmash");
 
-// Check connection
-if ($mysqli -> connect_errno) {
-  echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
-  exit();
-}
+// // Check connection
+// if ($mysqli -> connect_errno) {
+//   echo "Failed to connect to MySQL: " . $mysqli -> connect_error;
+//   exit();
+// }
 
-$id = $_GET['id'];
-// Perform query
-if ($result = $mysqli -> query("SELECT order_status FROM orders WHERE order_id > $id")) {
-  echo "Not empty data";
-}else{
-	echo "empty data";
-}
+// $id = $_GET['id'];
+// // Perform query
+// if ($result = $mysqli -> query("SELECT order_status FROM orders WHERE order_id > $id")) {
+//   echo "Not empty data";
+// }else{
+// 	echo "empty data";
+// }
 
-$mysqli -> close();
+// $mysqli -> close();
+echo $_GET['url'];
 ?>
